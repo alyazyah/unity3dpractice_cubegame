3e5858f482d5a964cee403dfefbc5bea85b1a78e
alyazyah <37453063+alyazyah@users.noreply.github.com>
2020-10-23 07:03:19 +0400

    fixed camera

--- practice project/Assets/PlayerMovement.cs
+++ practice project/Assets/PlayerMovement.cs
@@ -2,15 +2,23 @@
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-     	Debug.Log("Hello world");   
-    }
+	public Rigidbody rb; // reference to rigidbody component
+
+	public float forwardForce = 2000f;
+	public float sidewaysForce = 500f;
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate() // playing around with physics
     {
-        
+    	// add a forward force
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d")) {
+        	rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("a")) {
+        	rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
     }
 }
