af649a78913d20c5e59f5c404ccdea13cdb9f49e
alyazyah <37453063+alyazyah@users.noreply.github.com>
2020-10-23 06:44:25 +0400

    adding components

---
+++ practice project/Assets/PlayerMovement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+     	Debug.Log("Hello world");   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
